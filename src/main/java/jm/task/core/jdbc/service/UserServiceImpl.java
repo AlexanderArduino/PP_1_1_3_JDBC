2eb07d70c1059a4ec1077fce2c5910b26f4184e3
Anokhin <a.i.anohin.arduino@gmail.com>
2024-06-17 17:16:01 +0300

    Add connections on Util class Add toString in User Shared all, because don't setting .gitignore

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,16 +1,41 @@
 package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
     public void createUsersTable() {
+         String createUserTable = "CREATE TABLE `new_table` (\n" +
+                "  `id` int NOT NULL AUTO_INCREMENT,\n" +
+                "  `name` varchar(45) NOT NULL,\n" +
+                "  `lastName` varchar(45) NOT NULL,\n" +
+                "  PRIMARY KEY (`id`)\n" +
+                ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;";
+
+        Connection conn = Util.getConnection();
+        try {
+            Statement st =conn.createStatement();
+            st.execute(createUserTable);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
     public void dropUsersTable() {
-
+       String dropUsersTable = "DROP TABLE `new_table`;";
+        Connection conn = Util.getConnection();
+        try {
+            Statement st =conn.createStatement();
+            st.execute(dropUsersTable);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,41 @@
 package jm.task.core.jdbc.util;
 
+import com.mysql.cj.ServerVersion;
+import com.mysql.cj.Session;
+import com.mysql.cj.exceptions.ExceptionInterceptor;
+import com.mysql.cj.interceptors.QueryInterceptor;
+import com.mysql.cj.jdbc.*;
+import com.mysql.cj.jdbc.result.CachedResultSetMetaData;
+import com.mysql.cj.jdbc.result.ResultSetInternalMethods;
+import com.mysql.cj.protocol.ServerSessionStateController;
+
+import java.sql.*;
+import java.sql.Blob;
+import java.sql.CallableStatement;
+import java.sql.Clob;
+import java.sql.DatabaseMetaData;
+import java.sql.Driver;
+import java.sql.NClob;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.Executor;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static final String DB_URL = "jdbc:mysql://localhost:3306/database";
+    public static final String DB_USER = "root";
+    public static final String DB_PASS = "root";
+
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+            System.out.printf("Connection to  %s opened", DB_URL);
+        } catch (SQLException e) {
+            throw new RuntimeException("Не удалось подключиться к БД");
+        }
+        return conn;
+    }
+
+
 }

068ee23f186d1316124420eb9456953d17865c08
Anokhin <a.i.anohin.arduino@gmail.com>
2024-06-20 15:31:04 +0300

    Ready for check task 1.1.3 after Menthor comment's

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,97 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import javax.swing.plaf.nimbus.State;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
-public class UserDaoJDBCImpl implements UserDao {
+public class UserDaoJDBCImpl extends Util implements UserDao {
+    private Connection connection = getConnection();
+
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
+        StringBuilder creatUsersTableSQL = new StringBuilder();
+        creatUsersTableSQL.append("CREATE TABLE IF NOT EXISTS `users` (\n");
+        creatUsersTableSQL.append("`id` INT NOT NULL AUTO_INCREMENT,\n");
+        creatUsersTableSQL.append("`name` VARCHAR(45) NOT NULL,\n");
+        creatUsersTableSQL.append("`lastName` VARCHAR(45) NOT NULL,\n");
+        creatUsersTableSQL.append("`age` INT NOT NULL,\n");
+        creatUsersTableSQL.append("PRIMARY KEY (`id`)) \n");
+        creatUsersTableSQL.append("ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;");
 
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(creatUsersTableSQL.toString());
+            System.out.printf("\nТаблица %s была создана", "users");
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
-
+        String dropUserTableSQL = "DROP TABLE IF EXISTS users;";
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(dropUserTableSQL);
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        String saveUserSQL = "INSERT INTO `users` (name, lastName, age) VALUES (?,?,?);";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(saveUserSQL)) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
 
+            preparedStatement.executeUpdate();
+            System.out.printf("\nUser с именем %s был добавлен в базу данных", name);
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
-
+        String removeUserByIdSQL = "DELETE FROM users WHERE id = (?)";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(removeUserByIdSQL)) {
+            preparedStatement.setInt(1, (int) id);
+            System.out.printf("\nПользователь с id = %s был удален", id);
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> userList = new ArrayList<>();
+        String getAllUsersSQL = "SELECT * FROM users;";
+        try (Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery(getAllUsersSQL);
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId((long) resultSet.getInt(1));
+                user.setName(resultSet.getString(2));
+                user.setLastName(resultSet.getString(3));
+                user.setAge((byte) resultSet.getInt(4));
+                userList.add(user);
+            }
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+        return userList;
     }
 
     public void cleanUsersTable() {
-
+        String clearUsersTableSQL = "TRUNCATE TABLE users;";
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(clearUsersTableSQL);
+            System.out.println("\nТаблица users была полностью очищена");
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,110 +1,33 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.util.Util;
-
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
+    UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+
     public void createUsersTable() {
-        String tableName = "users";
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("CREATE TABLE IF NOT EXISTS ")
-                .append(tableName).append(" (")
-                .append("  `id` int NOT NULL AUTO_INCREMENT,\n")
-                .append("  `name` varchar(45) NOT NULL,\n")
-                .append("  `lastName` varchar(45) NOT NULL,\n")
-                .append("  `age` int NOT NULL, \n")
-                .append("  PRIMARY KEY (`id`)\n").append(") ")
-                .append("ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;");
-//        String createUserTable = "CREATE TABLE IF NOT EXISTS `new_table` (\n" +
-//                "  `id` int NOT NULL AUTO_INCREMENT,\n" +
-//                "  `name` varchar(45) NOT NULL,\n" +
-//                "  `lastName` varchar(45) NOT NULL,\n" +
-//                "  PRIMARY KEY (`id`)\n" +
-//                ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;";
-
-        try (Connection conn = Util.getConnection()){
-            Statement st =conn.createStatement();
-            st.execute(stringBuilder.toString());
-            System.out.printf("\nТаблица %s была создана", tableName);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
+        userDaoJDBC.createUsersTable();
     }
 
     public void dropUsersTable() {
-        String tableName = "users";
-        String dropUsersTable = "DROP TABLE IF EXISTS " + tableName + ";";
-
-        try (Connection conn = Util.getConnection()) {
-            Statement st =conn.createStatement();
-            st.execute(dropUsersTable);
-            System.out.println("\nТаблица " + tableName + " была удалена");
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        userDaoJDBC.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("INSERT INTO `users` (name, lastName, age) VALUES (")
-                        .append("\"").append(name).append("\", ")
-                        .append("\"").append(lastName).append("\", ")
-                        .append(age).append(");");
-//        stringBuilder.append("INSERT INTO `users` (`name`, `lastName`, `age`) VALUES ('Gosha', 'Pechkin', '20');");
-        try (Connection conn = Util.getConnection()){
-            Statement st =conn.createStatement();
-            st.execute(stringBuilder.toString());
-            System.out.printf("\nUser с именем %s был добавлен в базу данных", name);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        userDaoJDBC.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        String query = "DELETE FROM `users` WHERE id = " + id + ";";
-        try (Connection connection = Util.getConnection(); Statement statement = connection.createStatement()) {
-            statement.execute(query);
-            System.out.println("\nПользователь с id = " + id + " был удален");
-        }catch (SQLException e) {
-            e.printStackTrace();
-        }
+        userDaoJDBC.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        List<User> userList = new ArrayList<>();
-        String query = "select * FROM `users`;";
-
-        try (Connection conn = Util.getConnection(); Statement statement = conn.createStatement()){
-            ResultSet resultSet = statement.executeQuery(query);
-            while(resultSet.next()) {
-                User user = new User();
-                user.setId((long)resultSet.getInt(1));
-                user.setName(resultSet.getString(2));
-                user.setLastName(resultSet.getString(3));
-                user.setAge((byte)resultSet.getInt(4));
-                userList.add(user);
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return userList;
+        return userDaoJDBC.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        String query = "TRUNCATE TABLE users";
-        try (Connection connection = Util.getConnection(); Statement statement = connection.createStatement()) {
-            statement.executeUpdate(query);
-            System.out.println("\nТаблица users была полностью очищена");
-        }catch (SQLException e) {
-            e.printStackTrace();
-        }
+        userDaoJDBC.cleanUsersTable();
     }
 }
